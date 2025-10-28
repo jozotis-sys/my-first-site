2c969f01478da605c7a8d977c13b2ee8d13f91ee
jozotis-sys <jozotis@gmail.com>
2025-10-27 20:30:01 -0400

    Add files via upload

---
+++ my-first-site/Scripts/script.js
@@ -0,0 +1,23 @@
+// Run after the DOM is ready
+window.addEventListener('DOMContentLoaded', () => {
+const yearEl = document.getElementById('year');
+yearEl.textContent = new Date().getFullYear();
+
+
+const btn = document.getElementById('surprise-btn');
+const msg = document.getElementById('surprise-text');
+
+
+btn.addEventListener('click', () => {
+msg.hidden = !msg.hidden; // toggle visibility
+});
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+     // safe to use all your elements here
+  const facts = document.querySelector("#fun-facts");
+
+  facts.addEventListener("click", () => {
+    alert("You clicked the fun facts section!");
+  });
+});
